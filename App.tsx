e3471c41a0d9f4c221282fd9974e7e6bda56f4e7
bemin.shaker <beminshaker1@outlook.com>
2022-03-27 00:10:01 -0400

    authentication

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/login';
+import SignupScreen from './screens/signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/signup.tsx
+++ screens/signup.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Dimensions, Text, TextInput} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import MyButton from '../components/myButton';
-import { logInWithEmail } from '../services/firebase';
+import { signUpWithEmail } from '../services/firebase';
 
 type ScreenProps = {
   navigation: any
@@ -11,18 +11,23 @@ type ScreenProps = {
 export default function LoginScreen({ navigation }: ScreenProps) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
 
-  logInWithEmail(email, password);
+  signUpWithEmail(fName, lName, email, password);
 
   return (
     <>
     <StatusBar style="light" />
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text>Signup</Text>
+      <TextInput style={styles.textInput} placeholder='First Name' onChangeText={setfName} />
+      <TextInput style={styles.textInput} placeholder='Last Name' onChangeText={setlName} />
       <TextInput style={styles.textInput} placeholder='Email'  onChangeText={setEmail} />
       <TextInput style={styles.textInput} placeholder='Password' secureTextEntry onChangeText={setPassword} />
-      <MyButton type="primary" text="Login" size="large" onPressFn={logInWithEmail}/>
+      <MyButton style={styles.textInput} type="primary" text="Signup" size="medium" onPressFn={signUpWithEmail}/>
       <View style={{height: Dimensions.get('screen').width * 0.05}}></View>
+      <Text>Don't have an account? <MyButton  text="Login" onPressFn={() => navigation.navigate("Login")}/></Text>
     </View>
     </>
   );
