4b88723e8a33627d728155388bfd4675015b1cf5
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-04-05 06:41:04 -0400

    added category buttons

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, View, Dimensions, Text, TextInput} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { BottomNavigation, Searchbar, Headline } from 'react-native-paper';
+import { BottomNavigation, Searchbar, Headline, Button } from 'react-native-paper';
 import { getProducts } from '../services/firebase';
 import MyButton from '../components/myButton';
 
@@ -35,6 +35,12 @@ const HomeScreen = ({ navigation, route }: ScreenProps) => {
 
   const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
 
+  // Buttons (item categories)
+  <>
+    
+    
+  </>
+
   return (
     <>
     
@@ -45,6 +51,52 @@ const HomeScreen = ({ navigation, route }: ScreenProps) => {
       onChangeText={onChangeSearch}
       value={searchQuery}
     />
+
+    <View style={styles.categoryButtonView}>
+      <Button
+        icon="bed-empty"
+        onPress={() => console.log('Go to Furniture page')}
+        mode="contained"
+        compact={true}
+        contentStyle={{flexDirection: "column"}}
+        labelStyle={{fontSize: 12, color: "#000000"}}
+        style={{marginVertical: 5, marginLeft: 5, backgroundColor: "#d6d6d6"}}>
+        Furniture
+      </Button>
+      
+      <Button
+        icon="book"
+        onPress={() => console.log('Go to Books page')}
+        mode="contained"
+        compact={true}
+        contentStyle={{flexDirection: "column"}}
+        labelStyle={{fontSize: 12, color: "#000000"}}
+        style={{marginVertical: 5, marginLeft: 5, backgroundColor: "#d6d6d6"}}>
+        Books
+      </Button>
+
+      <Button
+        icon="hanger"
+        onPress={() => console.log('Go to Clothing page')}
+        mode="contained"
+        compact={true}
+        contentStyle={{flexDirection: "column"}}
+        labelStyle={{fontSize: 12, color: "#000000"}}
+        style={{marginVertical: 5, marginLeft: 5, backgroundColor: "#d6d6d6"}}>
+        Clothing
+      </Button>
+
+      <Button
+        icon="lightbulb"
+        onPress={() => console.log('Go to Electronics page')}
+        mode="contained"
+        compact={true}
+        contentStyle={{flexDirection: "column"}}
+        labelStyle={{fontSize: 12, color: "#000000"}}
+        style={{marginVertical: 5, marginLeft: 5, backgroundColor: "#d6d6d6"}}>
+        Electronics
+      </Button>
+    </View>
     
     <BottomNavigation
       navigationState={{ index, routes }}
@@ -71,5 +123,9 @@ const styles = StyleSheet.create({
     borderBottomColor: "#3A3A3A",
     borderBottomWidth: 1,
     padding: 10
+  },
+  categoryButtonView: {
+    flexDirection: "row",
+    justifyContent: "flex-start"
   },
 });
